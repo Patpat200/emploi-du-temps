96e3f17a6c04c26b725c723c001601cfd3f2e443
e2500465 <adilelb2007@gmail.com>
2026-09-11 11:34:23 +0200

    change id v2

--- app.config.ts
+++ app.config.ts
@@ -13,7 +13,7 @@ const schemeFromBundleId = `manus${timestamp}`;
 const env = {
   // App branding - update these values directly (do not use env vars)
   appName: "Emploi du Temps Etudiant",
-  appSlug: "emploi-du-temps-etudiant",
+  appSlug: "uha",
   // S3 URL of the app logo - set this to the URL returned by generate_image when creating custom logo
   // Leave empty to use the default icon from assets/images/icon.png
   logoUrl: "https://files.manuscdn.com/user_upload_by_module/session_file/310519663276306839/eXSSYLUysmkVntVW.png",
